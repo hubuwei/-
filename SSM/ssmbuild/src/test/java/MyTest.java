3af6cc5184d8cc58757ce5d87d58b3cf8fb91cc9
hubuwei <134253614+hubuwei@users.noreply.github.com>
2024-01-17 13:30:42 +0800

    Add files via upload

---
+++ SSM/ssmbuild/src/test/java/MyTest.java
@@ -0,0 +1,10 @@
+import com.cheng.service.BookService;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    @Test
+    public void test(){
+    }
+}
